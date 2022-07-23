f5852d3c16c1fa535125ebb57d79001ee1a80166
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-23 12:04:39 -0300

    test: remove test not used

--- devagro-frontend/src/app/app.component.spec.ts
+++ devagro-frontend/src/app/app.component.spec.ts
@@ -20,12 +20,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'devagro-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('devagro-frontend');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
