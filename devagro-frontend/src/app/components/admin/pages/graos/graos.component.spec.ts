711e3837fc8560c3bc9f16fc271f1b7ddbbc4743
thiagoperest <thiago_teodoroperes07@hotmail.com>
2022-07-24 18:41:26 -0300

    test: grain principal component

--- devagro-frontend/src/app/components/admin/pages/graos/graos.component.spec.ts
+++ devagro-frontend/src/app/components/admin/pages/graos/graos.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { RouterTestingModule } from '@angular/router/testing';
+
 import { GraosComponent } from './graos.component';
 
 describe('GraosComponent', () => {
@@ -8,6 +10,7 @@ describe('GraosComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [ GraosComponent ]
     })
     .compileComponents();
@@ -19,7 +22,39 @@ describe('GraosComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Componente criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Selecionar grão', () => {
+    const grao: any = {
+      selectedGrao: 'Grao',
+      mostrar: true
+    }
+    expect(component.onSelect(grao)).not.toBeNull();
+  });
+
+  it('Armazenamento do grão', () => {
+    expect(component.storeGrao).toBeTruthy();
+  });
+
+  it('Deletar grão específico', () => {
+    const grao: any = {
+      selectedGrao: 'Grao',
+      mostrar: true
+    }
+    expect(component.SpecificDelete(grao)).not.toBeNull();
+  });
+
+  it('Deletar grão', () => {
+    expect(component.deleteGraos).not.toBeNull();
+  });
+
+  it('Refresh page', () => {
+    expect(component.refreshPage).toBeTruthy();
+  });
+
+  it('Btn click', () => {
+    expect(component.btnClick).toBeTruthy();
   });
 });
